751c2eec5f129bf6e0b62962cf5ecd940b33ee2a
elizasquire <110359435+elizasquire@users.noreply.github.com>
2022-08-29 12:26:06 +1200

    I added html, with a header and a nav bar

--- weekly_special.php
+++ weekly_special.php
@@ -12,3 +12,24 @@ $all_ws_query = "SELECT ID, item_on_special, new_cost, Week from weekly_special
 ORDER BY Week ASC;"; /*ws stands for weekly special*/
 $all_ws_result = mysqli_query($dbcon, $all_ws_query);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php
+	echo "<h1>Our Weekly Specials"."</h1>";
+	?>
+</head>
+<body>
+<main>
+	<nav>
+		<ul>
+			<li> <a href="index_assessment.php">Home</a></li>
+			<li> <a href="menu.php">Our Menu</a></li>
+			<li> <a href="items.php">Items</a></li>
+			<li> <a href="weekly_special.php">Weekly Specials</a></li>
+		</ul>
+	</nav>
+</main>
+</body>
+</html>
