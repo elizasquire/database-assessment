649d81f2fa56f3fa73a19a9b1885a90301f95686
elizasquire <110359435+elizasquire@users.noreply.github.com>
2022-08-29 12:22:55 +1200

    I added my queries

--- weekly_special.php
+++ weekly_special.php
@@ -7,3 +7,8 @@ if(mysqli_connect_errno()){
 else {																   /* so my tables can be accessed */
 	echo "connected to database";}
 ?>
+<?php
+$all_ws_query = "SELECT ID, item_on_special, new_cost, Week from weekly_special
+ORDER BY Week ASC;"; /*ws stands for weekly special*/
+$all_ws_result = mysqli_query($dbcon, $all_ws_query);
+?>
