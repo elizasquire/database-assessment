bdb972547edfb7273ca3911793f2e38a6f662077
elizasquire <110359435+elizasquire@users.noreply.github.com>
2022-09-12 09:30:38 +1200

    I changed the table fromat so it now links in with menu page.
    I also removed the weekly special page from the nav bar so now it can be directly accessed through the ID field in the menu table.

--- weekly_special.php
+++ weekly_special.php
@@ -7,12 +7,21 @@ if(mysqli_connect_errno()){
 else {																   /* so my tables can be accessed */
 	echo "connected to database";}
 ?>
+
 <?php
 $all_ws_query = "SELECT ID, item_on_special, new_cost, Week from weekly_special
 ORDER BY Week ASC;"; /*ws stands for weekly special*/
 $all_ws_result = mysqli_query($dbcon, $all_ws_query);
 ?>
+<?php
 
+if(isset($_GET['ID'])){
+	$ID = $_GET['ID'];
+}else{
+	$ID = "0";
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,55 +32,48 @@ $all_ws_result = mysqli_query($dbcon, $all_ws_query);
 <body>
 <main>
 	<nav>
-		<ul>
-			<li> <a href="index_assessment.php">Home</a></li>
-			<li> <a href="menu.php">Our Menu</a></li>
-			<li> <a href="items.php">Items</a></li>
-			<li> <a href="weekly_special.php">Weekly Specials</a></li>
-		</ul>
-	</nav>
 
+			<a class="active" href="index_assessment.php">Home</a>
+  			<a href="menu.php">Menu</a>
+  			
+	</nav>
 <?php
 
-$result = mysqli_query($dbcon, $all_ws_query);
-if (!$result) 
-{
-	$message = 'ERROR: ' . mysqli_connect_error();
-	return $message;
-}
-else
-{
-	$i = 0;
-	echo '<html><body><table><tr>';
-	while ($i < mysqli_num_fields($result))
-	{
-		$tr = mysqli_fetch_field($result);
-		echo '<td>' . $tr->name . '</td>';
-		$i = $i + 1;
-	}
-	echo '</tr>';
-	
-	while ( ($row = mysqli_fetch_row($result))) 
-	{
-		$count = count($row);
-		$y = 0;
-		echo '<tr>';
-		while ($y < $count)
-		{
-			$c_row = current($row);
-			echo '<td>' . $c_row . '</td>';
-			next($row);
-			$y = $y + 1;
-		}
-		echo '</tr>';
-		
-	}
-	mysqli_free_result($result);
-	
-	echo '</table></body></html>';
+$sql = "SELECT * FROM weekly_special
+WHERE weekly_special.ID='$ID'"; 
 
-}
+
+$rs = mysqli_query($dbcon, $sql)
+or die ('Problem with query' . mysqli_error());
+
 ?>
+
+<table border="1" summary="Staff Orders">
+<tr>
+<th>ID</th>
+<th>Item on special</th>
+<th>New Cost</th>
+<th>Week on special</th>
+</tr>
+
+<?php
+while ($row = mysqli_fetch_array($rs)) { ?>
+
+<tr>
+
+<td><?php echo $row["ID"]?></td>
+<td><?php echo $row["item_on_special"]?></td>
+<td><?php echo $row["new_cost"]?></td>
+<td><?php echo $row["Week"]?></td>
+
+</tr>
+
+
+<?php   }
+ ?>
+</table>
+
+
 </main>
 </body>
 </html>
