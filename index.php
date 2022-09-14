da9bff1a05285da07fe34402f0a9d7e8871b84fb
elizasquire <110359435+elizasquire@users.noreply.github.com>
2022-09-14 13:14:47 +1200

    I made some final changes including removing the "Connected to database" messsage to display at the top of the browser, because I know that my webpage connects to the database now.

--- index.php
+++ index.php
@@ -1,37 +1,44 @@
 <?php
 /*Connect to database */
-$dbcon = mysqli_connect("localhost", "elizasquire", "CQYu2NF", "elizasquire_cafe");
+$dbcon = mysqli_connect("localhost", "elizasquire", "CQYu2NF", "elizasquire_1");
 
 if(mysqli_connect_errno()){
 	echo "Failed to connect to MySQL:".mysqli_connect_error(); die();} /*This is what connects the website to my database, so my tables can be accessed*/
 else {																   /* so my tables can be accessed */
-	echo "connected to database";
+	echo "";
 }
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title> WEGC Cafe </title>
+	<title>WEGC Cafe</title>
 </head>
 <body>	
 <?php
-echo "<h1>Welcome to WEGC Cafe "."</h1>";
+echo "<h2>Welcome to WEGC Cafe "."</h2>";
 
 ?>
+
 <main>
 	<header>
-		<h1>Options</h1>
+		<h3>Options</h3>
 	</header>
 	<nav>
   <a class="active" href="index_assessment.php">Home</a>
   <a href="menu.php">Menu</a>
-  <a href="weekly_special.php">Weekly Special</a>
 	</nav>
 	<?php
 	echo "<p>Here at WEGC we are committed to providing our students with cheap and well made food that relieves the stress of packed lunches.
 	</p>";
 
 	?>
+
+
+
 </main>
+
+
 </body>
 </html>
