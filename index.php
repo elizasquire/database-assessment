109207ee0fe252520a0545396d44b4c75ba79501
elizasquire <110359435+elizasquire@users.noreply.github.com>
2022-09-24 18:33:31 +1200

    Update index.php
    I put the intro paragraph into my css grid and decided to add the items link back to the nav bar

--- index.php
+++ index.php
@@ -13,6 +13,9 @@ else {																   /* so my tables can be accessed */
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<meta charset="utf-8">
+<link href="css/styles.css" rel="stylesheet">
+
 	<title>WEGC Cafe</title>
 </head>
 <body>	
@@ -25,16 +28,27 @@ echo "<h2>Welcome to WEGC Cafe "."</h2>";
 	<header>
 		<h3>Options</h3>
 	</header>
-	<nav>
-  <a class="active" href="index_assessment.php">Home</a>
-  <a href="menu.php">Menu</a>
-	</nav>
+	<div class = "ul  li a li a:hover">
+  
+			<nav>
+				<ul class="ul">
+					<li class="li"><a class="active" href="index_assessment.php">Home</a></li> <!-- Why is it not horizontal! -->
+					<li class="li"><a class="active" href="items.php">Items</a></li>
+					<li class = "li"><a class="active" href="menu.php">Menu</a></li>
+					<li class = "li"><a class="active" href="weekly_special.php">Weekly Specials</a></li>
+				</ul>
+			</nav>
+			</div>
+
+	<div class = "grid-item-c">
 	<?php
-	echo "<p>Here at WEGC we are committed to providing our students with cheap and well made food that relieves the stress of packed lunches.
+	echo "<p>Here at the WEGC cafe we are committed to providing our students with cheap and well made food that relieves the stress of packed lunches. We offer a wide range
+	of food and drinks for students to enjoy.
 	</p>";
 
 	?>
-
+		
+	</div>
 
 
 </main>
