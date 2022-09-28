254dbc800375ec882261a3230cd09b69dcc66709
elizasquire <110359435+elizasquire@users.noreply.github.com>
2022-09-28 21:49:52 +1300

    I added some more css specifically I added images and more detail to my paragraphs. I also created a footer in which I cited my images in.

--- index.php
+++ index.php
@@ -19,40 +19,58 @@ else {																   /* so my tables can be accessed */
 	<title>WEGC Cafe</title>
 </head>
 <body>	
+<div class="grid-container">
+<div class="header">
+<div class="h1">
 <?php
-echo "<h2>Welcome to WEGC Cafe "."</h2>";
+echo "<h1>Welcome to WEGC Cafe "."</h1>";
 
 ?>
+</div>
+</div>
 
-<main>
-	<header>
-		<h3>Options</h3>
-	</header>
+	<div class = "navbar">
 	<div class = "ul  li a li a:hover">
-  
+  	<h2>Options</h2>
+
 			<nav>
 				<ul class="ul">
-					<li class="li"><a class="active" href="index_assessment.php">Home</a></li> <!-- Why is it not horizontal! -->
-					<li class="li"><a class="active" href="items.php">Items</a></li>
+					<li class="li"><a class="active" href="index_assessment.php">Home</a></li> 
 					<li class = "li"><a class="active" href="menu.php">Menu</a></li>
+					<li class="li"><a class="active" href="items.php">Items</a></li>
 					<li class = "li"><a class="active" href="weekly_special.php">Weekly Specials</a></li>
 				</ul>
 			</nav>
 			</div>
-
-	<div class = "grid-item-c">
+	</div>
+	<div class = "index_p">
+	<div class="p">
 	<?php
 	echo "<p>Here at the WEGC cafe we are committed to providing our students with cheap and well made food that relieves the stress of packed lunches. We offer a wide range
-	of food and drinks for students to enjoy.
+	of food and drinks for students to enjoy. We offer cabinet and counter food, as well as cooked meals. We are a student and staff run organisation who are passionate about providing
+	a great experience for our customers. Our goals are to make new and current students feel safe and welcome in our WEGC community, by providing them with delicious and nutritious food
+	at reasonable prices.
+	
 	</p>";
 
 	?>
-		
 	</div>
+	</div>
+		<div class="img_div">
+			<img src="images/wegc.png" alt="wegc school logo" title="wegc school logo" width="250" height="250">	
+			<img src="images/croissant.jpg" alt="croissant" title="croissant" width="250" height="250">	
+		</div>
 
-
-</main>
-
-
+		
+	<div class="wrap">
+	<div class="footer">
+	<footer>
+		<p>&copy; 2022 E Squire. Picture of Wellington East Girls' College logo: sourced directly from wegc.school.nz. Please note, these images are merely stock images and should not be used as an example of what the items at the WEGC Cafe look like.</p>
+		
+		<p class="attribution">"<a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/photos/28669118@N00/4766528912">20100703 making croissants 16</a>" by <a target="_blank" rel="noopener noreferrer" 			href="https://www.flickr.com/photos/28669118@N00">JStove</a> is licensed under <a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/by-nd-nc/2.0/jp/?ref=openverse">CC BY-NC-ND 2.0 			<img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" style="height: 1em; margin-right: 0.125em; display: inline;"><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" 							style="height: 		1em; margin-right: 0.125em; display: inline;"><img src="https://mirrors.creativecommons.org/presskit/icons/nc.svg" style="height: 1em; margin-right: 0.125em; display: inline;"><img 							src="https://mirrors.creativecommons.org/presskit/icons/nd.svg" style="height: 1em; margin-right: 0.125em; display: inline;">.</p>
+	</footer>
+	</div>
+	</div>
+</div>
 </body>
 </html>
