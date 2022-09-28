72f70655da7a516d963dd7faaa71196ddd39b989
elizasquire <110359435+elizasquire@users.noreply.github.com>
2022-09-28 21:56:04 +1300

    I changed my items page so it now displays all the items together, while the individual item informatino can be accessed from the menu page which lead the user to the items_individual.php.
    I also added php to my header paragraph, and my  items table. I added two images and a footer.

--- items.php
+++ items.php
@@ -26,27 +26,51 @@ if(isset($_GET['Item'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<?php
-	echo "<h1>Items Information "."</h1>";
-	?>
+<meta charset="utf-8">
+<link href="css/styles.css" rel="stylesheet">
+	<title> WEGC Cafe </title>
+
 </head>
-<body>
-	<header>
-		<h1>Items</h1>
-	</header>
-	<nav>
-		<a class="active" href="index_assessment.php">Home</a>
-  		<a href="menu.php">Back to Menu</a>
-	</nav>
 	
+<body>
+<div class="grid-container">
+<div class="header">
+<div class="h1">
+<?php
+echo "<h1>Items"."</h1>";
+
+?>
+</div>
+</div>
+	 <div class="navbar">
+	 <div class = "ul li a li a:hover">
+		<div class="option">
+		<h2>Options</h2>
+		 </div>
+			<nav>
+				<ul class="ul">
+					<li class="li"><a class="active" href="index_assessment.php">Home</a></li> <!-- Why is it not horizontal! -->
+					<li class = "li"><a class="active" href="menu.php">Menu</a></li>
+					<li class="li"><a class="active" href="items.php">Items</a></li>
+					<li class = "li"><a class="active" href="weekly_special.php">Weekly Specials</a></li>
+				</ul>
+			</nav>
+			</div>
+		</div>
 
 
+<div class="wrap">
+<div class="img_div">	
+<img src="images/sushi.jpg" alt="sushi" title="sushi" width="320" height="320">
+<img src="images/brownie.jpg" alt="brownie" title="brownie" width="320" height="320">
 
-
+	</div>
+</div>
+		<div class = "menu">
+			
 <?php
 
-$sql = "SELECT * FROM Items
-WHERE Items.Item='$item'"; 
+$sql = "SELECT * FROM Items"; 
 
 
 $rs = mysqli_query($dbcon, $sql)
@@ -77,6 +101,22 @@ while ($row = mysqli_fetch_array($rs)) { ?>
 
 <?php   }
 mysqli_close($dbcon); ?>
+	<div class="p1">
+	 <form name='drinks_form' id='drinks_form' method='get' action='availability.php'>
+
+		<!--drink button-->
+		<input type='submit' name='drinks_button' value='Sort by availability'>
+	</form>
+	</div>
 </table>
+</div>
+	<div class="footer">
+		<footer>
+			<p> &copy; 2022 E Squire. Please note, these images are merely stock images and should not be used as an example of what the items at the WEGC Cafe look like.</p>
+			<p class="attribution">"<a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/photos/78779574@N00/8486536177">Best Salmon Sushi</a>" by <a target="_blank" rel="noopener noreferrer" 			href="https://www.flickr.com/photos/78779574@N00">Michael Kappel</a> is licensed under <a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/by-nc/2.0/?ref=openverse">CC BY-NC 2.0 <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" style="height: 1em; margin-right: 0.125em; display: inline;"><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" style="height: 1em; margin-right: 0.125em; display: inline;"><img src="https://mirrors.creativecommons.org/presskit/icons/nc.svg" style="height: 1em; margin-right: 0.125em; display: inline;">. </p>
+			<p class="attribution">"<a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/photos/45488928@N00/2975510440">Brownies</a>" by <a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/photos/45488928@N00">Cristiano Betta</a> is licensed under <a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/by/2.0/?ref=openverse">CC BY 2.0 <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" style="height: 1em; margin-right: 0.125em; display: inline;"><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" style="height: 1em; margin-right: 0.125em; display: inline;">. </p>
+	</footer>
+</div>
+</div>
 </body>
 </html>
