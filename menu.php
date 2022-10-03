1caa3f6f8204d43d740d27a81387df6cfea32df2
elizasquire <110359435+elizasquire@users.noreply.github.com>
2022-10-03 21:23:36 +1300

    I added some more comments and simplified the creditation in the footer for the creative commons images, so it was more easy to decipher rahter than the html credit code straight off the website.

--- menu.php
+++ menu.php
@@ -10,12 +10,13 @@ else {																   /* so my tables can be accessed */
 ?>
 <?php
 /* Menu Query */
-/*SELECT menu_id, FROM menu*/
+/*sets item as 1 so that a result will always be returned*/
 if(isset($_GET['Item'])){
 	$item_id = $_GET['Item'];
 }else{
 	$item_id = 1;
 }
+/*sets ID as 1 so that a result will always be returned*/
 if(isset($_GET['ID'])){
 	$id = $_GET['ID'];
 }else{
@@ -23,10 +24,9 @@ if(isset($_GET['ID'])){
 }
 ?>
 <?php
-$all_menu_query = "SELECT ID, Item FROM menu
-ORDER BY ID ASC;";
-$all_menu_result = mysqli_query($dbcon, $all_menu_query);
-$rs = mysqli_query($dbcon, $all_menu_query)
+$all_menu_query = "SELECT ID, Item FROM menu   
+ORDER BY ID ASC;";		/*query that calls the menu*/
+$rs = mysqli_query($dbcon, $all_menu_query)  /*connects the query to the database*/
 or die ('Problem with query' . mysqli_error());
 
 ?>
@@ -36,13 +36,13 @@ or die ('Problem with query' . mysqli_error());
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<link href="css/styles.css" rel="stylesheet">
+<link href="css/styles.css" rel="stylesheet"> <!-- Connects to stylesheet -->
 	<title> WEGC Cafe </title>
 
 </head>
 	
 <body>
-<div class="grid-container-menu">
+<div class="grid-container-menu">  <!-- specific grid container for menu page -->
 <div class="header">
 <div class="h1">
 <?php
@@ -53,12 +53,12 @@ echo "<h1>Menu"."</h1>";
 </div>
 
 	<div class="navbar">
-	 <div class = "ul li a li a:hover">
+	 <div class = "ul li a li a:hover">    <!-- nav bar divs-->
 		<h2>Options</h2>
 
 			<nav>
 				<ul class="ul">
-					<li class="li"><a class="active" href="index_assessment.php">Home</a></li> <!-- Why is it not horizontal! -->
+					<li class="li"><a class="active" href="index_assessment.php">Home</a></li>
 					<li class = "li"><a class="active" href="menu.php">Menu</a></li>
 					<li class="li"><a class="active" href="items.php">Items</a></li>
 					<li class = "li"><a class="active" href="weekly_special.php">Weekly Specials</a></li>
@@ -67,67 +67,73 @@ echo "<h1>Menu"."</h1>";
 			</div>
 		</div>
 <div class="img_div">	
-<img src="images/fruit_salad.jpg" alt="fruit salad" title="fruit salad" width="350" height="350">
+<img src="images/fruit_salad.jpg" alt="fruit salad" title="fruit salad" width="350" height="350"> <!--images-->
 <br>
 <br>
-<br>
+<br> <!-- break between images so there is a gap between them -->
 <br>
 <br>
 <img src="images/garlic_bread(1).jpg" alt="garlic bread" title="garlic bread" width="350" height="350">
 
 	</div>
 		<div class = "menu">
-
-			<table border="1" summary="Menu">
-			<tr>
-			<th>Item</th>
-			<th>ID</th>
-			</tr>
-	
-<?php
-while ($row = mysqli_fetch_array($rs)){ ?>
-<tr>
-<div class="menu_a">
-<td><a href="items_individual.php?Item=<?php echo $row["Item"]?>" style="text-decoration: none;" "color: #84CEEB;">
-    <?php echo $row["Item"]?></a>
-<td><a href="weekly_special_individual.php?ID=<?php echo $row["ID"]?>">
-	<?php echo $row["ID"]?></a>
-</div>
-</tr>
-<?php
-	}
-?>
- <form name='drinks_form' id='drinks_form' method='get' action='menu_sort.php'>
+		<div class="a:visited">
+<form name='drinks_form' id='drinks_form' method='get' action='menu_sort.php'> <!-- sort button that sorts the menu by cost when the user clicks on it -->
 
 		<!--drink button-->
 		<input type='submit' name='menu button' value='Sort by cost'>
 	</form>
 <?php
 	echo "<p>Click the item to find out more information!</p>";
-	echo "<p>Click the ID to find out when this is on special!</p>";
-	?>
+	echo "<p>Click the ID to find out when this is on special!</p>"; /* Tells the user how to use the links in the menu*/
+?>
+			
+			<table border="1">
+			<tr>
+			<th>Item</th>  <!-- names of the columns -->
+			<th>ID</th>
+			</tr>
+	
+<?php
+while ($row = mysqli_fetch_array($rs)){ ?>
+<tr>
+<td><a href="items_individual.php?Item=<?php echo $row["Item"]?>"><?php echo $row["Item"]?></a> <!--links the items to the page with the individual information about each item-->
+<td><a href="weekly_special_individual.php?ID=<?php echo $row["ID"]?>"> <!-- links the ids to the page with the weekly special information about that particular id that was clicked -->
+	<?php echo $row["ID"]?></a>
+</tr>
+
+<?php
+	}
+?>
 </table>
+ 
 </div>
-	<div class="aside">
+</div>
+
+	<div class="aside">  <!-- aside with extra information about wegc-->
 		<aside>
-			<img src="images/wegc.jpg" alt="front of wellington east girls' college" title="wellington east girls' college" width="350" height="350">
+			<img src="images/wegc.jpg" alt="front of wellington east girls' college" title="wellington east girls' college" width="350" height="350"> <!-- dimensions are equal so image is a square-->
 			<p> Wellington East Girls' College was founded in 1925 as the second girls' school in Wellington City, as the role at Wellington Girls' College was getting too large for staff to manage.
 				It now has its own role if 1058 pupils (as of July 2022). The WEGC cafe is situated in the new building Matairangi (pictured above) which was opened in 2019. It provides a large amount of
-				the sense of community at WEGC, as many pupils gather here during meal time while purchasing food. At WEGC Cafe, we are proud to offer a safe and happy environment in which students can congregate
-				while queuing up to get their food.</p>
+				the sense of community at WEGC, as many pupils gather here during meal time while purchasing food. At WEGC Cafe, we are proud to offer a safe and happy environment in which students can 					congregate while queuing up to get their food.</p>
 		</aside>
 	</div>
 	
-	<div class="footer">
+	<div class="footer"> <!-- footer div opens -->
 		<footer>
 			<p>&copy; 2022 E Squire. Please note, these images are merely stock images and should not be used as an example of what the items at the WEGC Cafe look like.</p>
-			<p class="attribution">"<a target="_blank" rel="noopener noreferrer" href="https://commons.wikimedia.org/w/index.php?curid=89490464">File:Wellington East Girls' College.jpg</a>" by <a target="_blank" 						rel="noopener noreferrer"
-			href="https://commons.wikimedia.org/wiki/User:Quilt_Phase">Tom Ackroyd</a> is licensed under <a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/by-sa/4.0/?								ref=openverse">CC BY-SA 4.0 <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" 
-			style="height: 1em; margin-right: 0.125em; display: inline;"><img 	src="https://mirrors.creativecommons.org/presskit/icons/by.svg" style="height: 1em; margin-right: 0.125em; display: inline;"><img 							src="https://mirrors.creativecommons.org/presskit/icons/sa.svg" style="height: 1em; margin-	right: 0.125em; display: inline;">. </p> <!--Credits from creative commons -->
-				
-			<p class="attribution">"<a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/photos/43053759@N04/5582818920">Fruit Salad</a>" by <a target="_blank" rel="noopener noreferrer" 									href="https://www.flickr.com/photos/43053759@N04">marktesta124</a> is licensed under <a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/by-nc-nd/2.0/?ref=openverse">CC BY-				NC-ND 2.0 <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" style="height: 1em; margin-right: 0.125em; display: inline;"><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" 				style="height: 1em; margin-right: 0.125em; display: inline;"><img src="https://mirrors.creativecommons.org/presskit/icons/nc.svg" style="height: 1em; margin-right: 0.125em; display: inline;"><img 							src="https://mirrors.creativecommons.org/presskit/icons/nd.svg" style="height: 1em; margin-right: 0.125em; display: inline;">.</p> <!-- Credit details for fruit salad image -->
+			<p> File:Wellington East Girls' College.jpg" by Tom Ackroyd is licensed under CC BY-SA 4.0. To view a copy of this license, visit:
+				<a href ="https://creativecommons.org/licenses/by-sa/4.0/?ref=openverse">https://creativecommons.org/licenses/by-sa/4.0/?ref=openverse</a></p> <!--Credits from creative commons -->
+				<!--crediting the image of wegc with a direct link to the information about the copyright license -->
+			<p>"Fruit Salad" by marktesta124 is licensed under CC BY-NC-ND 2.0. To view a copy of this license, visit: <a href ="https://creativecommons.org/licenses/by-nc-nd/2.0/?ref=openverse">
+				https://creativecommons.org/licenses/by-nc-nd/2.0/?ref=openverse</a></p>
+				<!-- crediting the image of the fruit salad from creative commons -->
 
-			<p class="attribution">"<a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/photos/87232391@N00/937700197">Garlic bread</a>" by <a target="_blank" rel="noopener noreferrer" 								href="https://www.flickr.com/photos/87232391@N00">Charles Haynes</a> is licensed under <a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/by-sa/2.0/?ref=openverse">CC BY-SA 2.0 <img 	  src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" style="height: 1em; margin-right: 0.125em; display: inline;"><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" style="height: 				1em; margin-right: 0.125em; display: inline;"><img src="https://mirrors.creativecommons.org/presskit/icons/sa.svg" style="height: 1em; margin-right: 0.125em; display: inline;">.</p>
+
+			<p>"Garlic bread" by Charles Haynes is licensed under CC BY-SA 2.0. To view a copy of this license, visit: <a href="https://creativecommons.org/licenses/by-sa/2.0/?ref=openverse">
+				https://creativecommons.org/licenses/by-sa/2.0/?ref=openverse</a></p>
+			<!-- crediting the image of the garlic bread from creative commons -->
+				
 		</footer>
 </div>
 </div>
